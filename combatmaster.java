fc868caec6abc721a3d7d7231383991d136e2e2f
BTobey <thenameisorca@gmail.com>
2018-08-30 19:49:59 -0700

    dead end
    dead end

--- combatmaster.java
+++ combatmaster.java
@@ -1,5 +1,7 @@
-public static void combat(dealer,target){
-        int CurrentHP=target.getHP();
-        CurrentHP=CurrentHP-dealer.getDmg();
-        target.setHP(CurrentHP);
-        }
+public static void class Combat(){
+    public void damage(dealer, target) {
+      int CurrentHP=target.getHP(); //gets targets hp
+      CurrentHP=CurrentHP-dealer.getDmg(); //subtracts dealer's damage from target's hp
+      target.setHP(CurrentHP); //uses target's setter to update with new health
+      }
+}
--- map.java
+++ map.java
@@ -3,6 +3,8 @@ BEGINNING OF GAME, THREE SIZES (NUMBER OF ROOMS FROM 5-7-9), THAT CONNECT
 TO EACHOTHER ONLY IN CARDINAL DIRECTIONS, NO REPEAT GENERATION OR OFF-MAP */
 
 import java.util.random;
+
+
 Random rand = new Random();
 int nextRoomCardinal = rand.nextInt(2) + 1;
 /* generates a random between 1-3, inclusively. (3 cardinal directions that
